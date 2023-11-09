0d0025144c102cd93cd67499c68dbd89d1118e3a
vjoshua365 <79820868+vjoshua365@users.noreply.github.com>
2023-11-09 18:13:06 -0500

    Update main.py
    change

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from string import punctuation, whitespace
+import itertools
 
+# task 1
 def words_extraction(path_to_file):
     # open file
+    words = []
     file = open(path_to_file, 'r+')
     outputfile = open("output_file.txt", "w")
 
@@ -16,9 +19,40 @@ def words_extraction(path_to_file):
                 if letter!=" ":
                     letter = letter.lower()
                 line+=letter
-        outputfile.writelines([line, "\n"])
+        words+=[line]
         file_line = file.readline()
     file.close()
+    outputfile.writelines(words)
+    return words
 
+words_extraction("test.txt")
 
-words_extraction('test.txt')
+def count_word(path_to_book):
+    # words is a list of strings, where each string is the lines of the book from perevious function
+    book = words_extraction(path_to_book)
+    Dictionary = dict()
+
+    # loop trhough lines in words variable for preface line to mark beginning of book
+    for i in range(len(book)):
+        # we found line
+        if "start of the project" in book[i]:
+            # ignore every previous line
+            book = book[i+1:]
+            break
+
+    for line in book:
+        # words is a list of each word in line
+        words = line.split(" ")
+        for word in words:
+            # remove whitespaces and new line symbols
+            word = word.replace("\n", "")
+            if word=="":
+                continue
+            # intialize count for word
+            if word not in Dictionary:
+                Dictionary[word]=0
+            # increment word frequency
+            Dictionary[word]+=1
+    return len(Dictionary)
+
+print(count_word("test.txt"))
